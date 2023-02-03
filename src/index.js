9ac85f2ff30601ba5946ca148253c2ffbacdcd47
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-02-03 11:00:50 -0400

    feat: add get and post endpoints

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 const express = require("express");
 const app = express();
+app.use(express.json());
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.get("/", (req, res) => {
+  res.contentType("application/json");
+  res.send({
+    message: "hello-world",
+  });
 });
 
 app.get("/hi", (req, res) => {
-  res.send("Hi!");
+  res.contentType("application/json");
+  res.send({
+    message: "hi",
+  });
+});
+
+const items = ["Rick Sanches", "Marty Smith", "Summer Smith"];
+
+app.get("/list", (req, res) => {
+  res.contentType("application/json");
+  res.send({ ...items });
+});
+
+app.get("/list/:id", (req, res) => {
+  res.contentType("application/json");
+  res.send({
+    item: items[req.params.id],
+  });
+});
+
+app.post("/list", (req, res) => {
+  items.push(...Object.values(req.body));
+  res.contentType("application/json");
+  res.send({ ...items });
 });
 
 app.listen(3001);
