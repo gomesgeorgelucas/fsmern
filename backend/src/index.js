527def730625f2fca141db1df12adc3f84e7cc10
George Lucas Gomes <gomes.georgelucas@gmail.com>
2023-02-24 09:12:32 -0400

    feat: changed mongo version and added env port

--- backend/src/index.js
+++ backend/src/index.js
@@ -82,5 +82,9 @@ const DB_NAME = "ocean-db-09-02-23";
     res.send(result);
   });
 
-  app.listen(3001);
+  const port = process.env.PORT || 3001;
+
+  app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+  });
 })();
